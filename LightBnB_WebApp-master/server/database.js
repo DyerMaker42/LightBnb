4ad31bee330e9850d552c87e5d88da632463ff53
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-08 23:40:51 +0000

    updating get all resos

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -83,7 +83,13 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+
+  return pool.query(`
+
+
+  `, [guest_id, limit])
+  .then( res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
